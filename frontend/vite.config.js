7076147b7fb87b2cf9d2230a37398530efe8f46c
Cypher Technologies <deploy@cyphertech.com>
2026-08-01 20:05:47 +0200

    Set Vite base path for GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/shein-with-rejo/',
   build: {
     outDir: 'dist',
     sourcemap: true,
