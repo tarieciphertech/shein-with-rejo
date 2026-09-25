bef80b22d2a5e873409184d4e14779b871348b8a
cyphertech <tariecipher@gmail.com>
2026-09-25 09:18:49 +0200

    Use root base for custom domain

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/shein-with-rejo/',
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: true,
